46313e362f8ef090018af4139bf00184add00572
JoseRuizCasado02 <joseblackstarx@gmail.com>
2020-05-05 18:14:28 +0200

    Side menu working. Not selectable options.

--- app/src/main/java/com/example/nbaanalyzer/MainActivity.kt
+++ app/src/main/java/com/example/nbaanalyzer/MainActivity.kt
@@ -2,11 +2,50 @@ package com.example.nbaanalyzer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.view.Menu
+import android.view.MenuItem
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addToolBar()
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
     }
+
+    private fun addToolBar() {
+        val toolbar = findViewById<Toolbar?>(R.id.tool_bar_main)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_dehaze_white_24dp)
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
