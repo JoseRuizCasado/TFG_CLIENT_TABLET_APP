85421c3bc24a01a05cb7c88bda5de42b3e1c0d69
JoseRuizCasado <53489806+JoseRuizCasado@users.noreply.github.com>
2020-08-21 18:17:17 +0200

    Bug solved.

--- app/src/main/java/com/example/nbaanalyzer/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/example/nbaanalyzer/ui/player/PlayerActivity.kt
@@ -5,13 +5,9 @@ import android.os.Bundle
 import android.view.MenuItem
 import com.example.nbaanalyzer.R
 import com.example.nbaanalyzer.TabsAdapter
-import com.example.nbaanalyzer.api.RestAPI
-import com.example.nbaanalyzer.api.ShotChartResponse
 import com.example.nbaanalyzer.ui.player.tabs.PlayerPreviewFragment
 import com.example.nbaanalyzer.ui.player.tabs.PlayerShotCharts
 import kotlinx.android.synthetic.main.activity_player.*
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
 import kotlin.properties.Delegates
 
 class PlayerActivity : AppCompatActivity() {
